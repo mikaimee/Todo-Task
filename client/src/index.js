1fc6b7de153640530d86d929dc1b4145713ca05e
Aimee Young <aim.mikoto@gmail.com>
2023-05-13 13:46:24 -0700

    added dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />}/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+    
   </React.StrictMode>
 );
 
