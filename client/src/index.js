58e37a86c8a5ad5136a2a36d50fc9448b9e840fd
Aimee Young <aim.mikoto@gmail.com>
2023-05-14 14:15:43 -0700

    minor changes

--- client/src/index.js
+++ client/src/index.js
@@ -2,22 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import { Provider } from 'react-redux';
-
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />}/>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-    
+    <App />
   </React.StrictMode>
 );
 
